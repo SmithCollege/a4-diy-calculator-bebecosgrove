ba9aad10ddb8059460537cabbc966b9e1bb13cf7
BebeCosgrove <bcosgrove@smith.edu>
2025-03-12 15:55:02 -0400

    more changes

--- app/src/main/java/a4/Infix.java
+++ app/src/main/java/a4/Infix.java
@@ -16,6 +16,7 @@ public class Infix {
         int num = tokens.size();
         HashMap<Character, Integer> operator_precedence = new HashMap<>(); // creates precedence values for different operators
 
+        operator_precedence.put('(', 5);
         operator_precedence.put('^', 4);
         operator_precedence.put('*', 3);
         operator_precedence.put('/', 3);
@@ -30,22 +31,24 @@ public class Infix {
             }
             else if ((Character) tokens.getFirst() == '('){
                 stack.push(tokens.getFirst());
+                System.out.println(stack);
                 tokens.removeFirst();
             }
             else if ((Character) tokens.getFirst() == ')') {
-                while ((Character) stack.peek() != '(' && stack.size() > 0){
+                System.out.println(stack);
+                while (stack.size() > 0 && (Character) stack.peek() != '('){
                     Object operator = stack.pop(); //pops elements that aren't left parenthesis
                     queue.add(operator); // pushes popped onto queue
                 }
                 if (stack.size() < 1){ // if stack is empty without finding left parentheses
-                    System.out.println("Mismatched parenthesis");
+                    System.out.println("Mismatched parenthesis2");
                 }
                 else if ((Character) stack.peek() == '('){
                     stack.pop(); // pop left parentheses from stack
                 }
                 tokens.removeFirst();
                 }
-            else if (tokens.getFirst() instanceof Character){
+            else if ((Character) tokens.getFirst() == '^' ||  (Character) tokens.getFirst() == '*' || (Character) tokens.getFirst() == '/' || (Character) tokens.getFirst() == '+' || (Character) tokens.getFirst() == '-'){
                 while (stack.peek() instanceof Character && operator_precedence.get(stack.peek()) >= operator_precedence.get(tokens.getFirst())){
                     Object stack_operator = stack.pop(); // pops last element on stack
                     queue.addLast(stack_operator); 
@@ -56,12 +59,12 @@ public class Infix {
             
             }
             while (stack.size() > 0){
-                if (stack.peek() == "(" || stack.peek() == ")"){
-                    System.out.println("Mismatched parentheses");
+                if ((Character) stack.peek() == '(' || (Character) stack.peek() == ')'){
+                    System.out.println("Mismatched parentheses1");
                 }
                 else if (stack.peek() instanceof Character){
                     Object operator = stack.pop();
-                    queue.add(operator);
+                    queue.addLast(operator);
                 }
             }
 
@@ -69,6 +72,8 @@ public class Infix {
             
         return(Postfix.postfix(queue)); // returns the postfix of the queue, which is a double
     }
+
+
 
     
 }
